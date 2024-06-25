c02943411565b1c9887edc682c8e0aef73a3bfd8
reactajit <95065796+reactajit@users.noreply.github.com>
2024-06-25 10:21:52 +0530

    Update task3.cy.js

--- cypress/e2e/task3.cy.js
+++ cypress/e2e/task3.cy.js
@@ -1,18 +1,24 @@
 
-describe('sc1', () => {
-
-    it.only('validate the title', () => {
-        cy.visit('https://react-shopping-cart-67954.firebaseapp.com/')
-        cy.get('.sc-bj2vay-1 hcyKTa[value='M']').click()
 
 
+//task3
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE 
+    w1.temperature > w2.temperature;
+//task4
+Inorder to have clarity on requirements i would ask following questions:
 
-
-
-    })
-
-
-
-})
-
+// What is the configured value for the number of previous passwords that cannot be reused?
+// Should the password history be tracked indefinitely or only for a certain period?
+// How will the system handle password changes made before the implementation of this feature?
+// Will the prompt question/answer be mandatory or optional for users?
+// Can users choose from a set of predefined questions, or can they create their own custom questions?
+// How many prompt questions/answers can a user set up? Is there a limit?
 
